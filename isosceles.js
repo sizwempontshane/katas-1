2ec7fabefba09b03a5edfcbe71cf7e69a1d4c484
sizwempontshane <61079347+sizwempontshane@users.noreply.github.com>
2020-08-28 11:44:31 +0200

    Delete isosceles.js

--- isosceles.js
+++
@@ -1,9 +0,0 @@
-function isosceles(val) {
-    for(var i=1; i<= val; i++){
-        var spaces = ' '.repeat(val-i); 
-        var hash = "#". repeat(i*2 -1)    
-      
-        console.log(spaces + hash);
-    }
-}
-isosceles(3);
