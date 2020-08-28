54716eed8bb7c897812d7726e77cb76658493c20
sizwempontshane <61079347+sizwempontshane@users.noreply.github.com>
2020-08-28 11:43:49 +0200

    Delete combine.js

--- combine.js
+++
@@ -1,9 +0,0 @@
-function combine(arr1, arr2) {
-   var arr3 = [];
-    
-   for (var w = 0; w < arr1.length || w < arr2.length; w++) {
-        arr3.push(arr1[w], arr2[w]);
-   }
-   console.log(arr3);
-}
-combine([11,22,33], [1,2,3]);
