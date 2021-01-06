bd4255035f697019c8d125a5dc4b00f077307f41
SizweSeize <mpontshanesizwe@gmail.com>
2021-01-06 12:09:41 +0200

    katas_redo

--- katas.js
+++ katas.js
@@ -5,37 +5,37 @@ function hello(name) {
 hello("Tshepo");
 
 // number is even or odd
-function evenOrodd(val) {
-  if (val % 2 === 0) {
+function evenOrodd(value) {
+  if (value % 2 === 0) {
     console.log("even");
   } else {
     console.log("odd");
   }
 }
-evenOrodd(50);
+evenOrodd(30);
 
-//a square
+//square
 function square(num) {
   str = "#";
   strRow = str.repeat(num) + "\n";
   return strRow.repeat(num);
 }
-console.log(square(5));
+console.log(square(4));
 
-//a right handed triangle
-function triangle(val) {
-  var hashs = [];
-  for (var t = 1; t <= val; t++) {
+//right handed triangle
+function triangle(value) {
+  var hashes = [];
+  for (var t = 1; t <= value; t++) {
     hashs += "#";
     console.log(hashs);
   }
 }
 triangle(4);
 
-//an isosceles triangle
-function isosceles(val) {
-  for (var i = 1; i <= val; i++) {
-    var spaces = " ".repeat(val - i);
+//isosceles triangle
+function isosceles(value) {
+  for (var i = 1; i <= value; i++) {
+    var spaces = " ".repeat(value - i);
     var hash = "#".repeat(i * 2 - 1);
 
     console.log(spaces + hash);
@@ -58,12 +58,12 @@ longest(["once", "upon", "a", "time"]);
 
 //combine two lists/arrays
 function combine(arr1, arr2) {
-  var arr3 = [];
+  var array3 = [];
 
   for (var w = 0; w < arr1.length || w < arr2.length; w++) {
-    arr3.push(arr1[w], arr2[w]);
+    array3.push(arr1[w], arr2[w]);
   }
-  console.log(arr3);
+  console.log(array3);
 }
 combine([11, 22, 33], [1, 2, 3]);
 
